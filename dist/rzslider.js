c5907d68966517597c2af5ffdb2d767c0bcc56b1
mdboop <mbrooks@opentable.com>
2016-06-29 17:16:18 -0700

    remove console log

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -1,6 +1,6 @@
-/*! angularjs-slider - v2.14.0 - 
- (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervieu.me>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> - 
- https://github.com/angular-slider/angularjs-slider - 
+/*! angularjs-slider - v2.14.0 -
+ (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervieu.me>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> -
+ https://github.com/angular-slider/angularjs-slider -
  2016-06-09 */
 /*jslint unparam: true */
 /*global angular: false, console: false, define, module */
@@ -2008,7 +2008,6 @@
         if (!localeConfig) {
           localeConfig = localeConfigList[defaultCountry];
         }
-        console.log("otPriceRangeSlider localeConfig:", localeConfig)
 
 
         // setup scope properties for use in rzslider
